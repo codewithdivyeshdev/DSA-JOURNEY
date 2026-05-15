144b466cb17eae54eb3efea8caf19131584ee2a5
Your Name <dabhidivyesh33@gmail.com>
2026-05-15 22:10:35 +0530

    method

---
+++ src/Day_13/methods.java
@@ -0,0 +1,13 @@
+package Day_13;
+
+public class methods {
+    public static int anybody(int a){
+        System.out.println("hello");
+        if(a>0) return 5;
+        else return 10;
+    }
+    public static void main(String[] args) {
+      int x=anybody(7);
+        System.out.println(3+x);
+    }
+}
